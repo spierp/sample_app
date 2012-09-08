3468785fa2061a6d87f65c396e83a8f85f115a81
Peter Spier <spierp@gmail.com>
2012-09-08 12:39:39 -0700

    static pages

---
+++ spec/requests/static_pages_spec.rb
@@ -0,0 +1,31 @@
+require 'spec_helper'
+
+  describe "About Page" do
+
+     it "should have the h1 'About'" do
+       visit '/static_pages/about'
+       page.should have_selector('h1', :text => 'About')
+     end
+     
+     it "should have the right Title" do
+       visit '/static_pages/about'  
+       page.should have_selector('title', :text => 'About')
+     end
+       
+     end
+ 
+     describe "Contact Page" do
+
+        it "should have the h1 'Contact'" do
+          visit '/static_pages/contact'
+          page.should have_selector('h1', :text => 'Contact')
+        end
+
+        it "should have the right Title" do
+          visit '/static_pages/contact'  
+          page.should have_selector('title', :text => 'Contact')
+        end
+
+        end
+
+
